d10e5d28c2d2953e34d22aa3b55c8610df7c5ea8
giuseppe <g.g@giggi.it>
2021-05-12 15:00:08 +0200

    adding restart always rule

--- script/nexus/runNexus.sh
+++ script/nexus/runNexus.sh
@@ -1,1 +1,1 @@
-docker run --detach --name CLANexus -p 8081:8081 --env-file nexus.env -v /dock/nexus:/nexus-data sonatype/nexus3:latest
+docker run --restart always --detach --name CLANexus -p 8081:8081 --env-file nexus.env -v /dock/nexus:/nexus-data sonatype/nexus3:latest
