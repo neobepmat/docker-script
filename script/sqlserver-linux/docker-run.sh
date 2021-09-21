503497812f75fefc9d1e0e709c3a49fb8a34dc60
giuseppe <giuseppe.grilli@cla-it.eu>
2021-09-21 10:40:06 +0200

    adding memory limits for sqlserver-2017-debian

--- script/sqlserver-linux/docker-run.sh
+++ script/sqlserver-linux/docker-run.sh
@@ -2,6 +2,10 @@ docker run --name sql2017e \
 --env "ACCEPT_EULA=Y" \
 --env "SA_PASSWORD=VivaLaRiga7GiorniSu7" \
 --env "MSSQL_PID=Express" \
+--memory="6g" \
+--memory-swap="6g" \
+--memory-reservation="4g" \
+--memory-swappiness="0" \
 -p 9433:1433 \
 --volume /Docker/sqlsrvr2017e/data/system:/var/opt/mssql \
 --volume /Docker/sqlsrvr2017e/data/user:/var/opt/sqlserver \
