c5d19d2792a900e11316594a05cbe969b504a86d
giuseppe <giuseppe.grilli@cla-it.eu>
2021-09-22 11:41:50 +0200

    adding sqlserver 2019 root-less container

---
+++ script/sqlserver-linux/docker-run-2019.sh
@@ -0,0 +1,13 @@
+docker run --name sql2019e \
+--env "ACCEPT_EULA=Y" \
+--env "SA_PASSWORD=VivaLaRiga7GiorniSu7" \
+--env "MSSQL_PID=Express" \
+--memory="6g" \
+--memory-swap="6g" \
+--memory-reservation="4g" \
+--memory-swappiness="0" \
+-p 9433:1433 \
+--volume /Docker/sqlsrvr2019e/data/system:/var/opt/mssql \
+--volume /Docker/sqlsrvr2019e/data/user:/var/opt/sqlserver \
+-d \
+mcr.microsoft.com/mssql/server:2019-GDR1-ubuntu-16.04
