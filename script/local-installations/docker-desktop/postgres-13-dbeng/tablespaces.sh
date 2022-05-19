c2c3832f5748f4d3b79f6c9d2f95b6ba516d03ef
neobepmat <giggiweb@yahoo.it>
2022-05-19 12:16:36 +0200

    adding creation of dbeng folders and tablespaces

--- script/local-installations/docker-desktop/postgres-13-dbeng/tablespaces.sh
+++ script/local-installations/docker-desktop/postgres-13-dbeng/tablespaces.sh
@@ -37,3 +37,48 @@ create_directory "TACO_M04B.DBF";
 
 echo "Creating tablespaces TACO_U.DBF"
 create_directory "TACO_U.DBF";
+
+
+echo "Creating DBENG tablespaces"
+
+echo "Creating tablespaces DBENG_M00A.DBF"
+create_directory "DBENG_M00A.DBF";
+
+echo "Creating tablespaces DBENG_M00B.DBF"
+create_directory "DBENG_M00B.DBF";
+
+echo "Creating tablespaces DBENG_M01A.DBF"
+create_directory "DBENG_M01A.DBF";
+
+echo "Creating tablespaces DBENG_M01B.DBF"
+create_directory "DBENG_M01B.DBF";
+
+echo "Creating tablespaces DBENG_M02A.DBF"
+create_directory "DBENG_M02A.DBF";
+
+echo "Creating tablespaces DBENG_M02B.DBF"
+create_directory "DBENG_M02B.DBF";
+
+echo "Creating tablespaces DBENG_M04A.DBF"
+create_directory "DBENG_M04A.DBF";
+
+echo "Creating tablespaces DBENG_M04B.DBF"
+create_directory "DBENG_M04B.DBF";
+
+echo "Creating tablespaces DBENG_M05A.DBF"
+create_directory "DBENG_M05A.DBF";
+
+echo "Creating tablespaces DBENG_M05B.DBF"
+create_directory "DBENG_M05B.DBF";
+
+echo "Creating tablespaces DBENG_M06A.DBF"
+create_directory "DBENG_M06A.DBF";
+
+echo "Creating tablespaces DBENG_M06B.DBF"
+create_directory "DBENG_M06B.DBF";
+
+echo "Creating tablespaces DBENG_M07A.DBF"
+create_directory "DBENG_M07A.DBF";
+
+echo "Creating tablespaces DBENG_M07B.DBF"
+create_directory "DBENG_M07B.DBF";
