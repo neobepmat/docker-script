9c80bfbecb83d6821fb63eba4508078f6459b858
neobepmat <giggiweb@yahoo.it>
2022-05-19 12:34:12 +0200

    fix recursive option

--- script/local-installations/docker-desktop/postgres-13-dbeng/prepare.sh
+++ script/local-installations/docker-desktop/postgres-13-dbeng/prepare.sh
@@ -1,6 +1,6 @@
 echo "Creating folders for Postgres 13 volumes"
+
 mkdir -p /Docker/pg13/data;
 mkdir -p /Docker/pg13/conf;
 
-chown mssql:docker /Docker/pg13 -r;
-
+chown mssql:docker /Docker/pg13 -R;
