79a6fe2993a1446ed1dffddd89e273780738cd2a
giuseppe <giuseppe.grilli@cla-it.eu>
2022-05-20 10:32:42 +0200

    final changes for local postgres 13

--- script/local-installations/docker-desktop/postgres-13-dbeng/prepare.sh
+++ script/local-installations/docker-desktop/postgres-13-dbeng/prepare.sh
@@ -2,5 +2,10 @@ echo "Creating folders for Postgres 13 volumes"
 
 mkdir -p /Docker/pg13/data;
 mkdir -p /Docker/pg13/conf;
+mkdir -p /Docker/pg13/preinitdb;
+mkdir -p /Docker/pg13/initdb;
 
-chown mssql:docker /Docker/pg13 -R;
+cp ./tablespaces.sh /Docker/pg13/preinitdb;
+cp ./scripts.sql /Docker/pg13/initdb;
+
+chown 1001:docker /Docker/pg13 -R;
