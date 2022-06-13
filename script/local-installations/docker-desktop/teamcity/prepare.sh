c81884436dacca747d444b293650f3b3f76d1df3
neobepmat <giggiweb@yahoo.it>
2022-06-13 15:56:22 +0200

    teamcity install on docker - local - docker desktop

---
+++ script/local-installations/docker-desktop/teamcity/prepare.sh
@@ -0,0 +1,10 @@
+TC_FOLDER=/Docker/teamcity
+
+echo Creating folders for data, configuration and basebackup
+mkdir -p $TC_FOLDER;
+
+mkdir -p $TC_FOLDER/data;
+mkdir -p $TG_FOLDER/logs;
+
+echo Setting user and group to folder
+chown 1000:1000 $TC_FOLDER -R
