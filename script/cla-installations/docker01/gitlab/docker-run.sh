cfcf1593f0132409d22287a36186111d4d4b1e22
neobepmat <giggiweb@yahoo.it>
2022-06-15 10:29:10 +0200

    updating gitlab, nexus for docker-01

---
+++ script/cla-installations/docker01/gitlab/docker-run.sh
@@ -0,0 +1,12 @@
+docker run \
+--restart always \
+--detach \
+--name CLAGitlab \
+--hostname docker-01.cla.cl-grp.local \
+--memory=10g \
+--memory-reservation=5g \
+--publish 4431:443 --publish 8082:80 --publish 222:22 \
+--volume /dock/gitlab/config:/etc/gitlab \
+--volume /dock/gitlab/logs:/var/log/gitlab \
+--volume /dock/gitlab/data:/var/opt/gitlab \
+gitlab/gitlab-ce:15.0.2-ce.0
